bb7854e396c2c9100284fd415bd4e221cb5fed81
tygedavis <tyge.davis@gmail.com>
2019-11-01 10:51:38 -0600

    Made updates for Tabs card. MVP met

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,43 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+function createCard(data){
+    //Create Element
+    const card = document.createElement('div'),
+          headline = document.createElement('div'),
+          author = document.createElement('div'),
+          imgContainer = document.createElement('div'),
+          img = document.createElement('img'),
+          authorsName = document.createElement('span');
+
+    //Add Classes
+    card.classList.add('card');
+    headline.classList.add('headline');
+    author.classList.add('author'),
+    imgContainer.classList.add('img-container');
+
+    //Organize Elements
+    card.append(headline, author, authorsName);
+    authorsName.appendChild(imgContainer);
+    imgContainer.appendChild(img);
+
+    //Add Content
+    headline.textContent = data.headline;
+    img.src = data.authorPhoto;
+    authorsName.textContent = `By ${data.authorName}`;
+
+    return card;
+}
+
+const cardsContainer = document.querySelector('.cards-container')
+
+
+
+axios
+    .get('https://lambda-times-backend.herokuapp.com/articles')
+    .then(res => {
+        console.log(res.data.articles);
+        const newCard = res.data.articles;
+        cardsContainer.appendChild(createCard(newCard))
+    })
